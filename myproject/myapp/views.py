32f3518634b7bb5b0f1b23f16065a874ad112b94
SteveRogersBD <aniruddhabiswasatanu@gmail.com>
2026-09-14 19:54:35 -0500

    a

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def hello(request):
     return HttpResponse("<h1>hello world</h1>")
 # Create your views here.
+def bye(request):
+    return HttpResponse("<h1>good bye</h1>")
